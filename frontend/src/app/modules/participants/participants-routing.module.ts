dde663f78e3d6f3b674a8756a8464fd0caefcbb8
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-07-22 09:13:47 +0300

    Add guard for participants/new route.

--- frontend/src/app/modules/participants/participants-routing.module.ts
+++ frontend/src/app/modules/participants/participants-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParticipantsComponent } from './participants.component';
-import {ParticipantDetailsComponent} from './components/participant-details/participant-details.component';
+import { ParticipantDetailsComponent } from './components/participant-details/participant-details.component';
+import { AdminGuard } from '@core/guards/admin.guard';
 
 const routes: Routes = [
   {path: '', component: ParticipantsComponent},
+  {path: 'new', component: ParticipantDetailsComponent, canActivate: [AdminGuard]},
   {path: ':id', component: ParticipantDetailsComponent},
 ];
 
