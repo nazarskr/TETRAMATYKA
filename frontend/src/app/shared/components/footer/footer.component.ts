06953d1b32fe7d2af93c045b376b625ad2e97818
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-08-05 21:22:23 +0300

    Add logos to footer. Fix header z-index.

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public footerImages = ['ucf_logo.png', 'lviv_logo.png', 'logo_lmr.png'];
 
   constructor() { }
 
