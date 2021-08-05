ad46a75dbccba5cd91328ace0cb06c25f38c180c
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-08-05 21:33:13 +0300

    Comment non-ready elements.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminGuard } from './core/guards/admin.guard';
+// import { AdminGuard } from './core/guards/admin.guard';
 
 const routes: Routes = [
   {
@@ -18,27 +18,27 @@ const routes: Routes = [
     loadChildren: () => import('./modules/program/program.module')
       .then(m => m.ProgramModule),
   },
-  {
-    path: 'projects',
-    loadChildren: () => import('./modules/projects/projects.module')
-      .then(m => m.ProjectsModule),
-  },
-  {
-    path: 'participants',
-    loadChildren: () => import('./modules/participants/participants.module')
-      .then(m => m.ParticipantsModule),
-  },
+  // {
+  //   path: 'projects',
+  //   loadChildren: () => import('./modules/projects/projects.module')
+  //     .then(m => m.ProjectsModule),
+  // },
+  // {
+  //   path: 'participants',
+  //   loadChildren: () => import('./modules/participants/participants.module')
+  //     .then(m => m.ParticipantsModule),
+  // },
   {
     path: 'contacts',
     loadChildren: () => import('./modules/contacts/contacts.module')
       .then(m => m.ContactsModule),
   },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin.module')
-      .then(m => m.AdminModule),
-    canActivate: [AdminGuard]
-  },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('./modules/admin/admin.module')
+  //     .then(m => m.AdminModule),
+  //   canActivate: [AdminGuard]
+  // },
   {
     path: 'page-not-found',
     loadChildren: () => import('./modules/page-not-found/page-not-found.module')
--- frontend/src/app/modules/home/home-routing.module.ts
+++ frontend/src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { NewsDetailsComponent } from './components/news-details/news-details.component';
+// import { NewsDetailsComponent } from './components/news-details/news-details.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'news/:id', component: NewsDetailsComponent}
+  // {path: 'news/:id', component: NewsDetailsComponent}
 ];
 
 @NgModule({
