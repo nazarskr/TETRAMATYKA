1633165427716239471a8961d6225e46812b2008
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-08-07 09:36:20 +0300

    Add logos to footer.

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  public footerImages = ['ucf_logo.png', 'lviv_logo.png', 'logo_lmr.png'];
+  public footerLogos = [
+    'NURT_logo.png', 'ucf_logo.png', 'lviv_logo.png',
+    'logo_lmr.png', 'CUH_Logo_ukr.png', 'sensoteka-lviv-logo.png',
+    'amp_logo.png', 'Lviv_misto_literatury.png', 'logo_zemla.png',
+    'EESEM_logo.png'
+  ];
 
   constructor() { }
 
