44aa5540878648139ad5728fb9ff127d9b3ff20b
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-08-14 08:57:05 +0300

    Add logo and poster for mobile. Grey supported by text. Fix border in home animation.

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { bigLogo } from './graphic/big-logo';
+import { verticalLogo } from './graphic/vertical-logo';
 
 @Component({
   selector: 'app-home',
@@ -10,36 +11,40 @@ import { bigLogo } from './graphic/big-logo';
 export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('logoContainer', {static: false}) logoContainer: ElementRef;
   public bigLogo: SafeHtml;
+  public verticalLogo: SafeHtml;
   public bubblesList = [];
 
   constructor(
     private _sanitizer: DomSanitizer
   ) {
     this.bigLogo = this._sanitizer.bypassSecurityTrustHtml(bigLogo);
+    this.verticalLogo = this._sanitizer.bypassSecurityTrustHtml(verticalLogo);
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    this.bubblesList = this.logoContainer.nativeElement.querySelectorAll('.st0');
-    this.bubblesList.forEach((item) => {
-      item.addEventListener('mouseover', () => {
-        if (!item.classList.contains('animated')) {
-          item.addEventListener("mousemove", () => {
-            item.style.transform = `translateY(-451px)`;
-          });
-          item.classList.add('animated');
-        }
-      });
+    if (this.logoContainer) {
+      this.bubblesList = this.logoContainer.nativeElement.querySelectorAll('.st0');
+      this.bubblesList.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+          if (!item.classList.contains('animated')) {
+            item.addEventListener("mousemove", () => {
+              item.style.transform = `translateY(-451px)`;
+            });
+            item.classList.add('animated');
+          }
+        });
 
-      item.addEventListener('animationend', () => {
-        if (item.classList.contains('animated')) {
-          item.classList.remove('animated');
-          item.style.transform = 'translate(0, 0)';
-        }
-      });
-    })
+        item.addEventListener('animationend', () => {
+          if (item.classList.contains('animated')) {
+            item.classList.remove('animated');
+            item.style.transform = 'translate(0, 0)';
+          }
+        });
+      })
+    }
   }
 
 }
