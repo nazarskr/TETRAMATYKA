a6df286b5c0d81c8e9d83f85dba5cd3d1a2536d2
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-09-02 18:19:23 +0300

    Add existing participant modal component.

---
+++ frontend/src/app/modules/works/components/existing-participant-modal/existing-participant-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-existing-participant-modal',
+  templateUrl: './existing-participant-modal.component.html',
+  styleUrls: ['./existing-participant-modal.component.scss']
+})
+export class ExistingParticipantModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
