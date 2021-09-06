1a2fe93d216b412a8611a90b8ef7651b0c211eee
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-09-06 17:30:02 +0300

    [FE] Fix token interceptor.

--- backend/src/common/interceptors/token.interceptor.ts
+++ backend/src/common/interceptors/token.interceptor.ts
@@ -7,7 +7,7 @@ export class TokenInterceptor<T> implements NestInterceptor<T> {
         const method = context.getArgs()[0].method;
         if (method !== 'GET') {
             const headers = context.getArgs()[0].headers;
-            if (headers.authorization === process.env.TOKEN) {
+            if (process.env.TOKEN && headers.authorization === process.env.TOKEN) {
                 return next.handle();
             } else {
                 throw new UnauthorizedException();
