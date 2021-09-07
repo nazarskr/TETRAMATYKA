0e38b938216d869ecc79df6697933f2df5a96250
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-09-07 11:24:42 +0300

    Add RadioGarage icon.

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -8,11 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   public footerLogos = [
     'NURT_logo.png', 'ucf_logo.png', 'logo_lmr.png', 'lviv_logo.png',
-    'CUH_Logo_ukr.png', 'logotype_LEM.png', 'sensoteka-lviv-logo.png',
-    'EESEM_logo.png', 'amp_logo.png',
-    'audio_art.png', 'Lviv_misto_literatury.png', 'logo_zemla.png',
-    'muzyka_centrum.png', 'krakow_logo.png', 'logo_iam.png',
-    'oead.png', 'IFU_UA.png'
+    'CUH_Logo_ukr.png', 'logotype_LEM.png', 'amp_logo.png',
+    'EESEM_logo.png', 'audio_art.png', 'Lviv_misto_literatury.png',
+    'logo_zemla.png', 'RadioGarage.png', 'muzyka_centrum.png',
+    'krakow_logo.png', 'logo_iam.png', 'oead.png', 'IFU_UA.png'
   ];
 
   constructor() { }
