e84921cc54a9fabe66791712b5321cbe83d8e592
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-10-09 08:45:30 +0300

    [CI/CD] Tests first.

--- build.sh
+++ build.sh
@@ -3,14 +3,16 @@ echo "Build starts..."
 cd backend
 echo "Installing backend dependencies..."
 npm install
-echo "Building backend..."
-npm run build-prod
-cd ../frontend
 echo "Installing frontend dependencies..."
+cd ../frontend
 npm install
-echo "Run tests..."
+echo "Run frontend tests..."
 npm run test-single
+echo "Building backend..."
+cd ../backend
+npm run build-prod
 echo "Building frontend..."
+cd ../frontend
 npm run build-prod
 
 
