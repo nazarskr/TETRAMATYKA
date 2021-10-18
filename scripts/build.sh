1f4fc30c2ebf7ecc1080fa9b7f4e2e545dc35d67
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-10-18 08:13:01 +0300

    Build commands.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ cd frontend
 npm run test-single
 echo "Building backend..."
 cd ..
-npm run build-prod
+npm run build:prod
 echo "Building frontend..."
 cd frontend
-npm run build-prod
+npm run build:prod
 
 
