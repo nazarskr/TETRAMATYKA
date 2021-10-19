7eed9e316c10ab29846b62e85546f27372f7f099
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-10-19 09:44:47 +0300

    Return build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,11 @@ echo "Backend tests should be here..."
 echo "Run frontend tests..."
 cd frontend
 npm run test-single
+echo "Building backend..."
+cd ..
+npm run build-prod
+echo "Building frontend..."
+cd frontend
+npm run build-prod
 
 
