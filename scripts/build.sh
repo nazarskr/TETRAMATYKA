46a2c4c9f7d23b826f61c35eb3c4181b4d036a97
nazarskr <nazarskrypnyk.programmer@gmail.com>
2021-10-19 08:49:21 +0300

    Move partners logos to separate folder. localization in forms. Remove build flow from travis.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,5 @@ echo "Backend tests should be here..."
 echo "Run frontend tests..."
 cd frontend
 npm run test-single
-echo "Building backend..."
-cd ..
-npm run build
-echo "Building frontend..."
-cd frontend
-npm run build:prod
 
 
