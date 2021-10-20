ed8b4a849b5937c4fe0b0656921a751954a7fdee
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2021-10-20 10:16:03 +0300

    [PWA] Reload location.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
 import { registerLocaleData } from '@angular/common';
 import localeUk from '@angular/common/locales/uk';
-import { SwUpdate } from "@angular/service-worker";
 import { ToasterService } from "@shared/services/toaster/toaster.service";
 import { UserService } from "@core/services/user.service";
 import { UserInfo } from "@shared/interfaces/user";
+import { ConnectionService } from 'ng-connection-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,17 @@ import { UserInfo } from "@shared/interfaces/user";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public isOnline = true;
 
   constructor(
     private _translateService: TranslateService,
     private _toaster: ToasterService,
-    private _userService: UserService
-  ) {}
+    private _userService: UserService,
+    private _connectionService: ConnectionService,
+    private _router: Router
+  ) {
+    this.detectConnectionStatus();
+  }
 
   ngOnInit(): void {
     registerLocaleData(localeUk, 'ua');
@@ -35,5 +41,22 @@ export class AppComponent implements OnInit {
           this._userService.changeUser(user);
         });
     }
+  }
+
+  detectConnectionStatus(): void {
+    this._connectionService.monitor().subscribe(isConnected => {
+      if (!this.isOnline && isConnected) {
+        this._toaster.showWarningMessage('You are online how');
+        const url = this._router.url;
+        if (url.includes('/offline') || url.includes('/page-not-found')) {
+          this._router.navigate(['/']);
+        } else {
+          window.location.reload();
+        }
+      } else {
+        this._toaster.showWarningMessage('You are offline now');
+      }
+      this.isOnline = isConnected;
+    })
   }
 }
