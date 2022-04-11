94d1c272b8ace92d30a4c141c41ce6b43bdd1ab6
Nazar Skrypnyk <nazar.skrypnyk@sombrainc.com>
2022-04-11 21:53:13 +0300

    Finished partners.

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -14,14 +14,6 @@ import {AddPartnersComponent} from "@shared/components/footer/add-partners/add-p
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent extends UnsubscribeOnDestroy implements OnInit {
-  public footerLogos = [
-    'NURT_logo.png', 'ucf_logo.png', 'logo_lmr.png', 'lviv_logo.png',
-    'CUH_Logo_ukr.png', 'logotype_LEM.png', 'amp_logo.png',
-    'EESEM_logo.png', 'audio_art.png', 'Lviv_misto_literatury.png',
-    'logo_zemla.png', 'RadioGarage.png', 'krakow_logo.png', 'muzyka_centrum.png',
-    'polish_institute.png', 'logo_iam.png', 'Ableton-Logo.png', 'oead.png', 'IFU_UA.png',
-  ];
-
   public partners: Partner[] = [];
 
   constructor(
