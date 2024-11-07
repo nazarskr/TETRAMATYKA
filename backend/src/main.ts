31b77cd077e4b1eaae62a34cf4a45e2f17284933
Skrypnyk <nazarskrypnyk@waterford.org>
2024-11-07 21:33:50 +0000

    added ssl config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface";
+import * as process from "process";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions: HttpsOptions = {
+    key: fs.readFileSync(process.env.S_KEY),
+    cert: fs.readFileSync(process.env.S_CERT),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.setGlobalPrefix('api');
-  await app.listen(80);
+  await app.listen(443);
 }
+
 bootstrap();
